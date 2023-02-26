44b27a32ad87f361f821218a62c57bce15561106
KacperKochanowski <kochanowski.k97@gmail.com>
2023-02-26 22:39:20 +0100

    removed fields used by Stadium class

--- src/main/java/com/mySportPage/model/Team.java
+++ src/main/java/com/mySportPage/model/Team.java
@@ -19,27 +19,7 @@ public class Team {
 
     private String country;
 
-    @JsonProperty("is_national")
-    private boolean isNational;
-
     @JsonProperty("founded")
     private Integer clubFounded;
-
-
-    @JsonProperty("venue_name")
-    private String stadium;
-
-    @JsonProperty(value = "venue_surface")
-    private String venueSurface;
-
-    @JsonProperty("venue_address")
-    private String addressOfStadium;
-
-    @JsonProperty("venue_city")
-    private String cityOfStadium;
-
-    @JsonProperty("venue_capacity")
-    private Long capacity;
-
 
 }
