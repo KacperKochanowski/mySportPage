39b556733d03e53209520f719b687eb337b7bf4e
KacperKochanowski <kochanowski.k97@gmail.com>
2023-02-27 22:03:37 +0100

    refactored methods to have separated one for teams and stadiums which come togheder in json

--- src/main/java/com/mySportPage/dataAcquisition/dao/DataAcquisitionDao.java
+++ src/main/java/com/mySportPage/dataAcquisition/dao/DataAcquisitionDao.java
@@ -1,6 +1,7 @@
 package com.mySportPage.dataAcquisition.dao;
 
 import com.mySportPage.model.SportObjectEnum;
+import com.mySportPage.model.Stadium;
 import com.mySportPage.model.Team;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,42 +32,52 @@ public class DataAcquisitionDao {
         this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
-    public void persistTeamsAndStadiums(List<Team> teams) {
-        if(teams.isEmpty()){
+    public void persistTeams(List<Team> teams) {
+        if (teams.isEmpty()) {
             return;
         }
-        MapSqlParameterSource parameters = new MapSqlParameterSource();
-        String persistTeams = "INSERT INTO public.teams (team_id, name, shortcut, club_crest, club_founded, country) " +
-                "VALUES (:externalTeamId, :name, :shortcut, :clubCrest, :clubFounded, :country); " +
-                "RETURNING ";
-
-        String persistStadium = "INSERT INTO public.stadiums (stadium, is_national, address_of_stadium, city_of_stadium, stadium_capacity, team_id) " +
-                "VALUES (:stadium, :isNational, :addressOfStadium, :cityOfStadium, :capacity, :externalTeamId);";
-
-
         for (Team team : teams) {
+            MapSqlParameterSource parameters = new MapSqlParameterSource();
             parameters.addValue("externalTeamId", team.getExternalTeamId() != null ? team.getExternalTeamId() : null);
             parameters.addValue("name", team.getName() != null ? team.getName() : null);
             parameters.addValue("shortcut", team.getShortCut() != null ? team.getShortCut() : null);
             parameters.addValue("clubCrest", team.getClubCrest() != null ? team.getClubCrest() : null);
             parameters.addValue("clubFounded", team.getClubFounded() != null ? team.getClubFounded() : null);
             parameters.addValue("country", team.getCountry() != null ? team.getCountry() : null);
-            parameters.addValue("stadium", team.getStadium() != null ? team.getStadium() : null);
-            parameters.addValue("isNational", team.isNational());
-            parameters.addValue("addressOfStadium", team.getAddressOfStadium() != null ? team.getAddressOfStadium() : null);
-            parameters.addValue("cityOfStadium", team.getCityOfStadium() != null ? team.getCityOfStadium() : null);
-            parameters.addValue("capacity", team.getCapacity() != null ? team.getCapacity() : null);
 
-            if(!checkIfObjectAlreadyExist(SportObjectEnum.TEAM, team.getExternalTeamId())) {
-                this.namedParameterJdbcTemplate.update(persistTeams, parameters);
+            String persistTeam = "INSERT INTO public.teams (team_id, name, shortcut, club_crest, club_founded, country) " +
+                    "VALUES (:externalTeamId, :name, :shortcut, :clubCrest, :clubFounded, :country);";
+
+            if (!checkIfObjectAlreadyExist(SportObjectEnum.TEAM, team.getExternalTeamId())) {
+                this.namedParameterJdbcTemplate.update(persistTeam, parameters);
                 log.info("Team: " + team.getName() + " stored in db.");
-            }
-            if(!checkIfObjectAlreadyExist(SportObjectEnum.STADIUM, team.getExternalTeamId())) {
-                this.namedParameterJdbcTemplate.update(persistStadium, parameters);
-                log.info("Stadium: " + team.getStadium() + " stored in db.");
             }
         }
     }
+
+    public void persistStadiums(List<Stadium> stadiums) {
+        if (stadiums.isEmpty()) {
+            return;
+        }
+        for (Stadium stadium : stadiums) {
+            MapSqlParameterSource parameters = new MapSqlParameterSource();
+            parameters.addValue("id", stadium.getId() != null ? stadium.getId() : null);
+            parameters.addValue("stadium", stadium.getStadium() != null ? stadium.getStadium() : null);
+            parameters.addValue("teamId", stadium.getExternalTeamId() != null ? stadium.getExternalTeamId() : null);
+            parameters.addValue("capacity", stadium.getCapacity() != null ? stadium.getCapacity() : null);
+            parameters.addValue("address", stadium.getAddress() != null ? stadium.getAddress() : null);
+            parameters.addValue("city", stadium.getCity() != null ? stadium.getCity() : null);
+
+            String persistStadium = "INSERT INTO public.stadiums (stadium_id, stadium, team_id, capacity, address, city) " +
+                    "VALUES (:id, :stadium, :teamId, :capacity, :address, :city);";
+
+            if (!checkIfObjectAlreadyExist(SportObjectEnum.STADIUM, stadium.getExternalTeamId())) {
+                this.namedParameterJdbcTemplate.update(persistStadium, parameters);
+                log.info("Stadium: " + stadium.getStadium() + " stored in db.");
+            }
+        }
+    }
+
 
     public boolean checkIfObjectAlreadyExist(SportObjectEnum object, Integer externalId) {
         MapSqlParameterSource parameters = new MapSqlParameterSource();
