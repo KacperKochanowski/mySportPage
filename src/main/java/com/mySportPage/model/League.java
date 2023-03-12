91561c6c831b06892e55d4eb4ed0cd3c5ddab3c5
KacperKochanowski <kochanowski.k97@gmail.com>
2023-03-12 19:21:42 +0100

    addded new object League

--- src/main/java/com/mySportPage/model/League.java
+++ src/main/java/com/mySportPage/model/League.java
@@ -1,2 +1,26 @@
-package com.mySportPage.model;public class League {
+package com.mySportPage.model;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class League {
+
+    private Integer externalLeagueId;
+
+    private String name;
+
+    private String type;
+
+    private String logo;
+
+    private Integer year;
+
+    private Date start;
+
+    private Date end;
+
+    private Country country;
+
 }
