d6262aa140144dd0135ac3334bfce6cb53cf7ad6
KacperKochanowski <kochanowski.k97@gmail.com>
2023-03-12 19:20:15 +0100

    addded three tables to DDL

---
+++ src/main/java/com/mySportPage/model/League.java
@@ -0,0 +1,2 @@
+package com.mySportPage.model;public class League {
+}
---
+++ src/main/java/com/mySportPage/model/LeagueCoverage.java
@@ -0,0 +1,31 @@
+package com.mySportPage.model;
+
+import lombok.Data;
+
+@Data
+public class LeagueCoverage {
+
+    private boolean events;
+
+    private boolean lineups;
+
+    private boolean statisticsFixtures;
+
+    private boolean statisticsPlayers;
+
+    private boolean standings;
+
+    private boolean players;
+
+    private boolean topScorers;
+
+    private boolean topAssists;
+
+    private boolean topCards;
+
+    private boolean injuries;
+
+    private boolean predictions;
+
+    private boolean odds;
+}
