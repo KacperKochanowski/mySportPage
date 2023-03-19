9da21e68f6496f2b33c66d37b69e1fcac4d1a307
KacperKochanowski <kochanowski.k97@gmail.com>
2023-03-19 17:54:39 +0100

    added additional constructor

--- src/main/java/com/mySportPage/dataAcquisition/model/Country.java
+++ src/main/java/com/mySportPage/dataAcquisition/model/Country.java
@@ -1,8 +1,12 @@
 package com.mySportPage.dataAcquisition.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Country {
 
     private String name;
@@ -10,4 +14,9 @@ public class Country {
     private String code;
 
     private String flag;
+
+
+    public Country(String name) {
+        this.name = name;
+    }
 }
