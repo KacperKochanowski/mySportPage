00e87d4729baafc9eb992039012416ccfbdcbdfa
KacperKochanowski <kochanowski.k97@gmail.com>
2023-03-19 17:55:33 +0100

    added specific constructor used during mapping data from json

--- src/main/java/com/mySportPage/dataAcquisition/model/Referee.java
+++ src/main/java/com/mySportPage/dataAcquisition/model/Referee.java
@@ -3,11 +3,24 @@ package com.mySportPage.dataAcquisition.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Referee {
 
     @JsonProperty("referee")
     private String name;
+
+    private Country country;
+
+    public Referee(String data) {
+        if(data != null) {
+            this.name = data.substring(0, data.lastIndexOf(","));
+            this.country = new Country(data.substring(data.lastIndexOf(",")));
+        } else {
+            new Referee();
+        }
+    }
 }
