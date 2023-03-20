12d9c66881d64b6bc79199d1a4afe132b033909f
KacperKochanowski <kochanowski.k97@gmail.com>
2023-03-20 21:41:50 +0100

    created another documentations for setters

--- src/main/java/com/mySportPage/dataAcquisition/model/Fixture.java
+++ src/main/java/com/mySportPage/dataAcquisition/model/Fixture.java
@@ -40,9 +40,22 @@ public class Fixture {
         this.finished = finished;
     }
 
+    /**
+     * Field is setting by concatenation of names of teams playing.
+     * Created to easily fetch and use event name.
+     * @param hostName - Match host (Team) name
+     * @param guestName - Match guest (Team) name
+     */
+
     public void setEvent(String hostName, String guestName) {
         this.event = String.format("%s - %s",hostName, guestName);
     }
+
+    /**
+     * It was decided to store the round field as a number to facilitate future processes on the database side.
+     * Feed provider forward this field like 'Regular Season - 1', but we use/need only digit from given value.
+     * @param round - Match day number during the season
+     */
 
     public void setRound(String round) {
         this.round = Integer.valueOf(round.replaceAll("\\D+", ""));
