aa56931592469b3fec156c96d303cfbf75559a6d
KacperKochanowski <kochanowski.k97@gmail.com>
2023-03-25 20:51:30 +0100

    added object for results

---
+++ src/main/java/com/mySportPage/dataAcquisition/model/Results.java
@@ -0,0 +1,16 @@
+package com.mySportPage.dataAcquisition.model;
+
+import java.util.Map;
+
+public class Results {
+
+    private Integer roundsPlayed;
+
+    private Integer wins;
+
+    private Integer draws;
+
+    private Integer loses;
+
+    private Map<String, Integer> goals;
+}
