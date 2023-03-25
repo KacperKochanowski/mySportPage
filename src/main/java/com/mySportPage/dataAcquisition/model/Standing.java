bdcc955c84c14f6dc7e5304dd887c46ec58405fe
KacperKochanowski <kochanowski.k97@gmail.com>
2023-03-25 20:53:44 +0100

    added standing object

---
+++ src/main/java/com/mySportPage/dataAcquisition/model/Standing.java
@@ -0,0 +1,28 @@
+package com.mySportPage.dataAcquisition.model;
+
+import java.util.Date;
+
+public class Standing {
+
+    private League league;
+
+    private Integer rank;
+
+    private Team team;
+
+    private Integer points;
+
+    private String goalsDiff;
+
+    private String form;
+
+    private String additionalPositionDescription;
+
+    private Results all;
+
+    private Results home;
+
+    private Results away;
+
+    private Date updated;
+}
