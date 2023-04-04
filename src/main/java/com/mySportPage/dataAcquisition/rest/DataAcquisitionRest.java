2438dc61fabc96b762214c5e8df95e850605663a
KacperKochanowski <kochanowski.k97@gmail.com>
2023-04-04 22:51:26 +0200

    changed from @Controller to @RestController

--- src/main/java/com/mySportPage/dataAcquisition/rest/DataAcquisitionRest.java
+++ src/main/java/com/mySportPage/dataAcquisition/rest/DataAcquisitionRest.java
@@ -10,12 +10,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
-@Controller
+@RestController
 @RequestMapping("internal/data-acquisition")
 public class DataAcquisitionRest {
 
