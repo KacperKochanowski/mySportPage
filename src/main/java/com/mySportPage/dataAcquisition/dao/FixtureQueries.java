7077d29b6b22bce95021c8f0133f1f1b860af616
KacperKochanowski <kochanowski.k97@gmail.com>
2023-04-08 15:13:27 +0200

    refactored query

--- src/main/java/com/mySportPage/dataAcquisition/dao/FixtureQueries.java
+++ src/main/java/com/mySportPage/dataAcquisition/dao/FixtureQueries.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 @Getter
 public enum FixtureQueries {
 
-    GET_FIXTURES(" SELECT event, round, result" +
-                 " FROM fixture" +
-                 " GROUP BY event, round, result" +
-                 " HAVING round = MIN(CAST((SELECT round FROM fixture WHERE played = false LIMIT 1) AS integer))");
+    GET_FIXTURES("SELECT league_id, event, start, played, result, round " +
+                 "FROM fixture " +
+                 "GROUP BY event, start, result, round, league_id, played " +
+                 "HAVING round = MIN(CAST((SELECT round FROM fixture WHERE played = false LIMIT 1) AS integer)) ");
 
     private final String query;
 
