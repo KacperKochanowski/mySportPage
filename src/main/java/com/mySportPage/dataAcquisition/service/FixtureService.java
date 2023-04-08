70493afc077b6b359226e27156cd9912e5cb6498
KacperKochanowski <kochanowski.k97@gmail.com>
2023-04-08 14:38:09 +0200

    added service for fixtures

---
+++ src/main/java/com/mySportPage/dataAcquisition/service/FixtureService.java
@@ -0,0 +1,24 @@
+package com.mySportPage.dataAcquisition.service;
+
+import com.mySportPage.dataAcquisition.dao.FixtureDao;
+import com.mySportPage.dataAcquisition.model.Fixture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FixtureService {
+
+    private static final Logger log = LoggerFactory.getLogger(FixtureService.class);
+
+    @Autowired
+    private FixtureDao fixtureDao;
+
+    public List<Fixture> getFixtures() {
+        return fixtureDao.getFixtures();
+    }
+
+}
