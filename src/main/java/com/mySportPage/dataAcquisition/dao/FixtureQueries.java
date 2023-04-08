18130572676f304fc5b169254dd4e6a5a3a26946
KacperKochanowski <kochanowski.k97@gmail.com>
2023-04-08 20:39:23 +0200

    added query

--- src/main/java/com/mySportPage/dataAcquisition/dao/FixtureQueries.java
+++ src/main/java/com/mySportPage/dataAcquisition/dao/FixtureQueries.java
@@ -12,8 +12,11 @@ public enum FixtureQueries {
     GET_FIXTURES("SELECT league_id, event, start, played, result, round " +
                  "FROM fixture " +
                  "GROUP BY event, start, result, round, league_id, played " +
-                 "HAVING round = MIN(CAST((SELECT round FROM fixture WHERE played = false LIMIT 1) AS integer)) ");
+                 "HAVING round = MIN(CAST((SELECT round FROM fixture WHERE played = false LIMIT 1) AS integer)) "),
 
+    GET_FIXTURES_BY_LEAGUE_ID("SELECT league_id, event, start, played, result, round " +
+                              "FROM fixture " +
+                              "WHERE league_id = :leagueId ");
     private final String query;
 
     FixtureQueries(String query) {
