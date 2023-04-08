67ef276c49d3b092b298df1e159c68f8918e54a0
KacperKochanowski <kochanowski.k97@gmail.com>
2023-04-08 20:38:01 +0200

    removed Query from no args method and overrided it by adding method params

--- src/main/java/com/mySportPage/dataAcquisition/dao/FixtureDao.java
+++ src/main/java/com/mySportPage/dataAcquisition/dao/FixtureDao.java
@@ -6,9 +6,8 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class FixtureDao {
@@ -18,9 +17,7 @@ public class FixtureDao {
 
     public List<FixtureDTO> getFixtures() {
         List<FixtureDTO> fixtureList = new ArrayList<>();
-
-        Query query = entityManager.createNativeQuery(FixtureQueries.GET_FIXTURES.getQuery());
-        List<Object[]> results = query.getResultList();
+        List<Object[]> results = entityManager.createNativeQuery(FixtureQueries.GET_FIXTURES.getQuery()).getResultList();
         for (Object[] value : results) {
             FixtureDTO fixture = new FixtureDTO();
             fixture.setLeagueId((Integer) value[0]);
@@ -32,5 +29,31 @@ public class FixtureDao {
             fixtureList.add(fixture);
         }
         return fixtureList;
+    }
+
+    public Map<Integer, List<FixtureDTO>> getFixtures(Integer leagueId, Integer round) {
+        List<FixtureDTO> list = new ArrayList<>();
+        String query = FixtureQueries.GET_FIXTURES_BY_LEAGUE_ID.getQuery();
+        Query queryObject;
+        if (round != null && round > 0) {
+            query += query + "AND round = :round ";
+            queryObject = entityManager.createNativeQuery(query);
+            queryObject.setParameter("round", round);
+        } else {
+            queryObject = entityManager.createNativeQuery(query);
+        }
+        queryObject.setParameter("leagueId", leagueId);
+        List<Object[]> results = queryObject.getResultList();
+        for (Object[] value : results) {
+            FixtureDTO fixture = new FixtureDTO();
+            fixture.setLeagueId((Integer) value[0]);
+            fixture.setEvent((String) value[1]);
+            fixture.setStart((Date) value[2]);
+            fixture.setFinished((boolean) value[3]);
+            fixture.setResult((String) value[4]);
+            fixture.setRound((Integer) value[5]);
+            list.add(fixture);
+        }
+        return list.stream().collect(Collectors.groupingBy(FixtureDTO::getRound));
     }
 }
