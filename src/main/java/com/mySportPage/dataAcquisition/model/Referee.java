e61a18354ed9322468647d9be98bcd4b3173d79e
KacperKochanowski <kochanowski.k97@gmail.com>
2023-04-10 21:50:49 +0200

    added description to constructor and modified logic due to wierd data

--- src/main/java/com/mySportPage/dataAcquisition/model/Referee.java
+++ src/main/java/com/mySportPage/dataAcquisition/model/Referee.java
@@ -15,10 +15,16 @@ public class Referee {
 
     private Country country;
 
+
+    /**
+     * Data forwarded by feed provider is unpredictable sometimes therefore wierd constructor needs to be used.
+     */
     public Referee(String data) {
-        if(data != null) {
+        if(data != null && data.contains(",")) {
             this.name = data.substring(0, data.lastIndexOf(","));
             this.country = new Country(data.substring(data.lastIndexOf(",")));
+        } else if (data != null) {
+            this.name = data;
         } else {
             new Referee();
         }
