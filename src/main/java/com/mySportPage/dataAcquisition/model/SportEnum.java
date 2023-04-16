ac278ba358a365739ae63c9165f41f84b6472850
KacperKochanowski <kochanowski.k97@gmail.com>
2023-04-16 15:56:22 +0200

    modified sportEnum

--- src/main/java/com/mySportPage/dataAcquisition/model/SportEnum.java
+++ src/main/java/com/mySportPage/dataAcquisition/model/SportEnum.java
@@ -7,11 +7,20 @@ public enum SportEnum {
 
     FOOTBALL(1, "football");
 
-    private final int id;
+    private final Integer id;
     private final String schema;
 
     SportEnum(int id, String schema) {
         this.id = id;
         this.schema = schema;
+    }
+
+    public static SportEnum getById(Integer id) {
+        for (SportEnum value : SportEnum.values()) {
+            if(value.id.equals(id)){
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid sportEnum id!");
     }
 }
