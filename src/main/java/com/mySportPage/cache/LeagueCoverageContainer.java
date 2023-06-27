445f56bcbe3ffa45db20b505b8b35fee426270c5
KacperKochanowski <kochanowski.k97@gmail.com>
2023-06-27 20:36:35 +0200

    added annotation @Scope("singleton")

--- src/main/java/com/mySportPage/cache/LeagueCoverageContainer.java
+++ src/main/java/com/mySportPage/cache/LeagueCoverageContainer.java
@@ -2,11 +2,13 @@ package com.mySportPage.cache;
 
 import com.mySportPage.model.LeagueCoverage;
 import com.mySportPage.model.SportEnum;
+import org.springframework.context.annotation.Scope;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Scope("singleton")
 public class LeagueCoverageContainer {
 
     private static Map<SportEnum, List<LeagueCoverage>> leagueCoverage = new HashMap<>();
