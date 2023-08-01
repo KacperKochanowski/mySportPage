27dee8572bf8e5ef492566855fcbb10d6ec63970
KacperKochanowski <kochanowski.k97@gmail.com>
2023-08-01 19:39:08 +0200

    implemented not including nullable values

--- src/main/java/com/mySportPage/model/dto/FixtureDTO.java
+++ src/main/java/com/mySportPage/model/dto/FixtureDTO.java
@@ -1,5 +1,6 @@
 package com.mySportPage.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,7 @@ import java.util.Date;
 @Builder(setterPrefix = "with")
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FixtureDTO {
 
     private Integer leagueId;
