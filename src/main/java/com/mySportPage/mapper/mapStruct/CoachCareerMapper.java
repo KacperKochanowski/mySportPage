4a2c74cda2fa1ddeefffbb38f2337ad2943a03fb
KacperKochanowski <kochanowski.k97@gmail.com>
2023-08-06 12:29:46 +0200

    added basic MapStruct mapper

---
+++ src/main/java/com/mySportPage/mapper/mapStruct/CoachCareerMapper.java
@@ -0,0 +1,34 @@
+package com.mySportPage.mapper.mapStruct;
+
+import com.mySportPage.model.CoachCareer;
+import com.mySportPage.model.Team;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface CoachCareerMapper {
+
+    default List<CoachCareer> mapToCoachCareerList(List<Object[]> data) {
+        return data.stream()
+                .map(this::mapToCoachCareerMapperModel)
+                .collect(Collectors.toList());
+    }
+
+    default CoachCareer mapToCoachCareerMapperModel(Object[] value) {
+        if (value == null || value.length < 4) {
+            return null;
+        }
+
+        CoachCareer model = new CoachCareer();
+        model.setTeam(new Team((Integer) value[0], ((String) value[1])));
+        model.setStart((Date) value[2]);
+        model.setEnd((Date) value[3]);
+
+        return model;
+    }
+}
