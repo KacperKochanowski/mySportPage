9b5727039ffdaa26210d49d943e05c8e910048ba
KacperKochanowski <kochanowski.k97@gmail.com>
2023-10-22 22:23:52 +0200

    added another date to Formatter

--- src/main/java/com/mySportPage/comonTools/Formatter.java
+++ src/main/java/com/mySportPage/comonTools/Formatter.java
@@ -7,6 +7,7 @@ public class Formatter {
 
     public static final SimpleDateFormat dateTimeNano = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
     public static final SimpleDateFormat dateTimeMin = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    public static final SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
 
     public static String parseDate(Date date, SimpleDateFormat dateFormat) {
         return dateFormat.format(date);
