592e3ae6c070052d8f604871b37bce8ba8d1c731
KacperKochanowski <kochanowski.k97@gmail.com>
2023-11-19 22:29:01 +0100

    added missing backslash for tests

--- src/main/java/com/mySportPage/rest/path/internal/FixtureStatisticsRestPath.java
+++ src/main/java/com/mySportPage/rest/path/internal/FixtureStatisticsRestPath.java
@@ -4,6 +4,6 @@ import static com.mySportPage.rest.path.internal.CommonRestParams.FIXTURE_ID;
 
 public interface FixtureStatisticsRestPath {
 
-    String ROOT_PATH = "fixture/statistics";
+    String ROOT_PATH = "/fixture/statistics";
     String GET_BY_FIXTURE_ID = "/id/{" + FIXTURE_ID + "}";
 }
